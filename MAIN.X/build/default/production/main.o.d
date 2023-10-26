6898404c506e0eabaf881a476b72aa3b77f75472
Vishnu C S <146208505+vishnu11x@users.noreply.github.com>
2023-10-27 03:53:40 +0530

    all files

---
+++ MAIN.X/build/default/production/main.o.d
@@ -0,0 +1,52 @@
+build/default/production/main.o.d build/default/production/main.o: main.c \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\avr\io.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\avr\sfr_defs.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\inttypes.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\features.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\stdint.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\bits\alltypes.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\bits\stdint.h \
+ E:/PROGRAM\ FILES/SOFTWARE/Microchip/MPLABX/v6.15/packs/Microchip/ATmega_DFP/3.0.158/xc8/avr/include/avr/iom328p.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\avr\portpins.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\avr\common.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\avr\fuse.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\avr\lock.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\util\delay.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\util\delay_basic.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\math.h \
+ e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\stdio.h
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\avr\io.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\avr\sfr_defs.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\inttypes.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\features.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\stdint.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\bits\alltypes.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\bits\stdint.h:
+
+E:/PROGRAM\ FILES/SOFTWARE/Microchip/MPLABX/v6.15/packs/Microchip/ATmega_DFP/3.0.158/xc8/avr/include/avr/iom328p.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\avr\portpins.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\avr\common.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\avr\fuse.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\avr\lock.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\util\delay.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\util\delay_basic.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\math.h:
+
+e:\program\ files\software\microchip\xc8\v2.45\avr\avr\include\stdio.h:
